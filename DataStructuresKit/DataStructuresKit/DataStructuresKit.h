d6c730a8ed505a0e4450de5f46c9bb1c1c30dfad
Shawn Webster <shawn@shawnwebster.com>
2015-06-25 21:13:20 +1000

    fixing workspace files

---
+++ DataStructuresKit/DataStructuresKit/DataStructuresKit.h
@@ -0,0 +1,19 @@
+//
+//  DataStructuresKit.h
+//  DataStructuresKit
+//
+//  Created by Shawn Webster on 24/06/2015.
+//  Copyright © 2015 Shawn Webster. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DataStructuresKit.
+FOUNDATION_EXPORT double DataStructuresKitVersionNumber;
+
+//! Project version string for DataStructuresKit.
+FOUNDATION_EXPORT const unsigned char DataStructuresKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DataStructuresKit/PublicHeader.h>
+
+
