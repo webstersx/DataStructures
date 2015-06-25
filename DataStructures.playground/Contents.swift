450b4a97ac42f90a5301556426155ecfc79e9998
Shawn Webster <shawn@shawnwebster.com>
2015-06-25 22:14:00 +1000

    playing with generic'd linked list

--- DataStructures.playground/Contents.swift
+++ DataStructures.playground/Contents.swift
@@ -4,20 +4,66 @@ import UIKit
 
 import DataStructuresKit
 
+/* //Link Lists
 var ll = LinkedList()
-
-var x = ll.head
-var y = ll.tail
-
-ll.add(1)
-ll.add(2)
-
+ll.size()
+ll.head
+ll.tail
 
 ll.find(1)
+ll.contains(1)
+ll.add(1)
+ll.size()
+ll.find(1)
+ll.contains(1)
 
+ll.head
+ll.tail
+
+ll.add(2)
+ll.size()
+
+ll.head
+ll.tail
+ll.head?.next
+ll.tail?.prev
+*/
+
+let i1 = LinkedListItem<Int>(object: 1)
+let i2 = LinkedListItem<Int>(object: 2)
+i1.next = i2
+i2.prev = i1
+
+i1.object
+i2.object
+i1.next?.prev?.next?.object
+
+let ll = LinkedList<Int>()
+let first = ll.add(1)
+let second = ll.add(1)
+
+first
+second
+
+first == second
+first === second
+first.object == second.object
+
+
+let firstFound = ll.find(1)!
+
+first == firstFound
+first === firstFound
+second == firstFound
+second === firstFound
+
+
+ll.head?.object
+ll.tail?.object
+ll.find(1)?.object
+ll.find(2)?.object
+ll.contains(1)
 ll.contains(2)
-
-
 
 
 
